a323ef20cd89842b1d6e0c5e9f6085597ba26e8e
Rafael Finol <rafaelfinol88@gmail.com>
2023-07-22 19:36:45 -0400

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return True if self.value.endswith(".") else False
+
+    def is_question(self):
+        return True if self.value.endswith("?") else False
+
+    def is_exclamation(self):
+        return True if self.value.endswith("!") else False
+
+    def count_sentences(self):
+        sentences = self.value.replace("!", ".").replace("?", ".").split(".")
+        no_empty_sentences = [i for i in sentences if i]
+        return len(no_empty_sentences)
+
+    value = property(get_value, set_value)
